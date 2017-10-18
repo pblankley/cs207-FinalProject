94e1134f2582d0d4e89e10866bb1618d4ba31001
pblankley <31221512+pblankley@users.noreply.github.com>
2017-10-18 09:54:11 -0400

    try setting matplotlib backend

--- code/demo.py
+++ code/demo.py
@@ -8,7 +8,14 @@ Created on Mon Oct 16
 
 import numpy as np
 import chemkin as ck
+import os
+
+import matplotlib as mpl
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
 import matplotlib.pyplot as plt
+
 
 # Demo of chemkin module
 # Parses rxns.xml and calculates chemical reaction rates for each of the six species

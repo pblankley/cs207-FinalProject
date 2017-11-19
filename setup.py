00e6dee74ce66d52b88e6bec639c01c8d3f00c27
pblankley <31221512+pblankley@users.noreply.github.com>
2017-11-19 12:45:17 -0500

    update version for tarball in setup

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name = 'chemkin207',
     packages = ['chemkin'],
-    version = '0.1.1',
+    version = '0.1.2',
     description = 'Simple chemical kinetics library.',
     author = 'Paul Blankley, Ryan Janssen, Boyuan Sun',
     author_email = 'pblankley@g.harvard.edu',
     copyright = 'MIT license',
     url = 'https://github.com/cs207-g1/cs207-FinalProject',
-    download_url = 'https://github.com/cs207-g1/cs207-FinalProject/tarball/0.1.0',
+    download_url = 'https://github.com/cs207-g1/cs207-FinalProject/tarball/0.1.2',
     keywords = ['chemical kinetics'],
     license='MIT',
     classifiers = [

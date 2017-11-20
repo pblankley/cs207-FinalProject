430a9a2b522712055eceee90f1dc8a482136ab2f
pblankley <31221512+pblankley@users.noreply.github.com>
2017-11-19 19:38:14 -0500

    add spaces

--- chemkin207/_tester.py
+++ chemkin207/_tester.py
@@ -4,13 +4,12 @@ import sys
 BASE_PATH = os.path.dirname(os.path.dirname(__file__))
 
 def test():
-    try: 
-	import pytest
+    try:
+	    import pytest
     except ImportError:
-	raise ImportError("You need pytest>=3.0 to run these tests")
+	    raise ImportError("You need pytest>=3.0 to run these tests")
     cmd = BASE_PATH
     print("running: pytest {}".format(cmd))
     sys.exit(pytest.main(cmd))
 
 __all__ = ['test']
-

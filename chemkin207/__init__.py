6ec154fc6acd34dac91ee537562efb5c0ada6723
pblankley <31221512+pblankley@users.noreply.github.com>
2017-11-19 19:36:00 -0500

    add test method

--- chemkin207/__init__.py
+++ chemkin207/__init__.py
@@ -2,3 +2,4 @@ from .chemkin207 import ReactionSet
 from .chemkin207 import IrreversibleReaction
 from .chemkin207 import ReversibleReaction
 from .chemkin207 import Reaction
+from ._tester import test
---
+++ chemkin207/_tester.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+BASE_PATH = os.path.dirname(os.path.dirname(__file__))
+
+def test():
+    try: 
+	import pytest
+    except ImportError:
+	raise ImportError("You need pytest>=3.0 to run these tests")
+    cmd = BASE_PATH
+    print("running: pytest {}".format(cmd))
+    sys.exit(pytest.main(cmd))
+
+__all__ = ['test']
+
